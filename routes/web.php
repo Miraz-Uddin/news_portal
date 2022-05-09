449a2cc874e8991e6b18d5c9e8cd89d719eb319e
Md. Miraz Uddin <mirazuddin0623@gmail.com>
2022-05-09 12:10:56 +0600

    Default Authentication Jetstream installed

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,13 @@ Route::get('/', function () {
 });
  
 Route::get('/content', [CheckAgeController::class, 'index'])->middleware('check');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
