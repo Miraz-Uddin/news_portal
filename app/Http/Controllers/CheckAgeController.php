546e390cefa5312fc034976029ce4664024d5aac
Md. Miraz Uddin <mirazuddin0623@gmail.com>
2022-05-09 11:38:55 +0600

    Check Age Controller Created

---
+++ app/Http/Controllers/CheckAgeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CheckAgeController extends Controller
+{
+    public function index(){
+        echo "This is Content page";
+    }
+}
